ad754f35a35767c6a28708fb161fff5daf49fb06
Vireak San <vsan93@yahoo.com>
2022-09-27 14:37:24 -0500

    authentication pages are not built and displaying, with minimalistic css added to it

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,9 +1,21 @@
 import './sign-up.styles.scss'
+import FormInput from '../form-input/form-input.component'
 
 const Signup = () =>{
     return(
         <div className="signup-container">
-            <h1>This is where we will be able to sign up</h1>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
+            <form>
+                <FormInput label="Display Name" type="text" required  name="displayName" value = ""/>
+
+                <FormInput label="Email" type="email" required  name ="email" value =""/>
+
+                <FormInput label="Password" type="password" required  name ="password" value =""/>
+
+                <FormInput label="Confirm Password" type= "password" required  name ="confirmPassword" value=""/>
+                <button type = "submit">Sign Up</button>
+            </form>
         </div>
     )
 }
