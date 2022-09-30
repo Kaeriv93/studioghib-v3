fe684331372769f9c37aafe94a82fc2f7b45a893
Vireak San <vsan93@yahoo.com>
2022-09-30 11:23:25 -0500

    all detail from our api is working flawlessly, will now style the showpages and then move on to authentication

--- src/components/film-details/film-details.components.jsx
+++ src/components/film-details/film-details.components.jsx
@@ -1,12 +1,22 @@
+import './film-details.styles.scss';
 const FilmDetails = ({film}) => {
     console.log(film)
     const details = () => {
         const {id, title, original_title, image, release_date, rt_score, running_time, producer, description, director, movie_banner} = film;
         return(
             <div key = {id} className="detail-container">
-                <img src = {movie_banner} alt ={title}/>
-                <h1>{title}</h1>
-                <h2>{original_title}</h2>
+                <div className="banner">
+                    <img src = {movie_banner} alt ={title}/>
+                </div>
+                <h1>Original Title: {original_title}</h1>
+                <h2>English Title: {title}</h2>
+                <img src = {image} alt = {title}/>
+                <h2>{release_date}</h2>
+                <h2>{rt_score}</h2>
+                <p>{running_time}</p>
+                <p>{producer}</p>
+                <p>{director}</p>
+                <p>{description}</p>
             </div>
         )
     }
